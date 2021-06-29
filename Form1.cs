9ad98512ac29fc0ae246ee6d41986ff5670a1826
Omar <omarzahir12@gmail.com>
2021-06-29 16:42:53 -0400

    added login functionality

--- Form1.cs
+++ Form1.cs
@@ -25,8 +25,22 @@ namespace FirstDatabase
 
         private void login_Click(object sender, EventArgs e)
         {
-            SqlConnection con = new SqlConnection("Data Source=DESKTOP-N8QVAOP; Database=Test_Database_1; ");
-            con = 
+            SqlConnection con = new SqlConnection("Data Source=DESKTOP-N8QVAOP; Database=Test_Database_1; Integrated Security=True");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Select * from Accounts_Table Where Username='" + txtUsername.Text + "' and Password ='" + txtPassword.Text + "'", con);
+            SqlDataReader sddr = cmd.ExecuteReader();
+
+            if (sddr.Read())
+            {
+                Dashboard ds = new Dashboard();
+                ds.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Incorrect Username or Password", "Message Title", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            con.Close();
         }
 
         private void Form1_Load(object sender, EventArgs e)
